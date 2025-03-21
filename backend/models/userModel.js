731c637db59a3415f46b09ab37eb4aa636521707
EnticG1 <nael.sing@gmail.com>
2025-03-22 03:29:09 +0700

    Added email and password validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,19 +18,34 @@ const userSchema = new Schema({
 
 // Static signup method
 userSchema.statics.signup = async function(email, password){
+
+    // Validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+
+    // Check if email is valid
+    if(!validator.isEmail(email)){
+        throw Error('Email is invalid')
+    }
+
+    // Check if password is strong enough (8 Characters min, special character, uppercase & lowercase, and number)
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough')
+    }
+
+    // Check if email already exists
     const exists = await this.findOne({ email })
 
     if(exists){
         throw Error('Email already exists')
     }
 
-    // Salting
-    const salt = await bcrypt.genSalt(8)
-    // Hash
-    const hash = await bcrypt.hash(password, salt)
+    // Encryption
+    const salt = await bcrypt.genSalt(8) // Salting
+    const hash = await bcrypt.hash(password, salt) // Hashing
 
-    const user = await this.create({ email, password: hash})
-
+    const user = await this.create({email, password: hash})
     return user
 }
 
